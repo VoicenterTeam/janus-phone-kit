78e15ac7d02e6ca9389296b7dd6552e2738f1b61
serhii.ku <serhii.ku@voicenter.com>
2023-12-20 17:49:14 +0200

    Added videoroom route for static room

---
+++ example/pages/VideoRoomConference.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <VcLoading :active="initializingData" full-page />
+
+    <JoinRoomModal
+      v-model="roomDetailsModel"
+      v-if="stateData.appConfig.PARAMETERS_MISSING_BEHAVIOR.type === ParametersMissingBehaviorType.REQUEST_IF_MISSING"
+      v-model:modal-visible="roomDetailsModalOpened"
+      :fields-to-show="['displayName']"
+      @submit="setRoomDetailsData"
+    />
+
+    <Conferencing v-if="roomJoined" @hangup="redirectToHomePage" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, inject } from 'vue'
+import { RouteLocationNormalizedLoaded, useRoute, useRouter } from 'vue-router'
+import { VIDEO_ROOM_CONFERENCE_PAGE_ROUTE, VIDEO_ROOM_ROUTE } from '@/router'
+//import { generateConferenceQueryParameters, getConferenceQueryParameters } from '@/helper/router.helper'
+import useJanusPhoneKit from '@/composables/useJanusPhoneKit'
+import Conferencing from '@/components/Conferencing.vue'
+import JoinRoomModal from '@/components/JoinRoomModal.vue'
+import { JoinRoomData } from '@/types/forms'
+import { ParametersMissingBehaviorType } from '@/config/app.config'
+import { ConfigInjectionKey } from '@/plugins/config'
+import { isQueryParameterValid } from '../helper/router.helper'
+import { CONFERENCE_PAGE_QUERY_PARAMETERS } from '@/enum/router.enum'
+
+/* Inject */
+const stateData = inject(ConfigInjectionKey)
+
+/* Composables */
+const route = useRoute()
+const router = useRouter()
+const { joinRoom } = useJanusPhoneKit()
+
+/* Data */
+const roomJoined = ref(false)
+const initializingData = ref(false)
+const roomDetailsModalOpened = ref(false)
+const roomDetailsModel = ref<JoinRoomData>({
+    roomId: 1234,
+    displayName: 'User'
+})
+
+/* Methods */
+function generateConferenceQueryParameters (displayName: string) {
+    return {
+        [CONFERENCE_PAGE_QUERY_PARAMETERS.DISPLAY_NAME]: displayName
+    }
+}
+function getConferenceQueryParameters (route: RouteLocationNormalizedLoaded): Partial<JoinRoomData> {
+    const routeQueryParameters = route.query
+
+    const paramsData: { roomId: number, displayName: string } = {
+        roomId: undefined,
+        displayName: undefined
+    }
+
+    //const roomId = routeQueryParameters[CONFERENCE_PAGE_QUERY_PARAMETERS.ROOM_ID]
+    const displayName = routeQueryParameters[CONFERENCE_PAGE_QUERY_PARAMETERS.DISPLAY_NAME]
+
+    /*if (isQueryParameterValid(roomId)) {
+        paramsData.roomId = Number(roomId)
+    }*/
+    paramsData.roomId = 1234
+
+    if (isQueryParameterValid(displayName)) {
+        paramsData.displayName = displayName
+    }
+
+    return paramsData
+}
+function redirectToHomePage () {
+    router.push({ name: VIDEO_ROOM_ROUTE.name })
+}
+async function joinConference (roomId: number, displayName: string) {
+    try {
+        await joinRoom({
+            roomId,
+            displayName,
+            mediaConstraints: {
+                audio: true,
+                video: true,
+            }
+        })
+
+        roomJoined.value = true
+        initializingData.value = false
+    } catch (e) {
+        return redirectToHomePage()
+    }
+}
+function checkIfOtherFieldsFilled (data: Partial<JoinRoomData>, fields: Array<keyof JoinRoomData>): boolean {
+    const uniqueFields = [ ...new Set(fields) ]
+
+    return Object.keys(data).every((key) => {
+        const dataKey = key as keyof JoinRoomData
+
+        if (uniqueFields.includes(dataKey)) {
+            return true // Skip fields that are in the 'fields' array
+        }
+
+        return data[dataKey] !== undefined // Check that all other fields are not undefined
+    })
+}
+
+
+function tryInitializeByQueryParameters () {
+    initializingData.value = true
+
+    const data = getConferenceQueryParameters(route)
+
+    if (data.roomId !== undefined && data.displayName !== undefined) {
+        joinConference(data.roomId, data.displayName)
+
+        return
+    }
+
+    switch (stateData.value.appConfig.PARAMETERS_MISSING_BEHAVIOR.type) {
+        case ParametersMissingBehaviorType.REDIRECT_TO_URL:
+            console.log('redirect')
+            console.log('REDIRECT_TO_URL')
+            window.location.href = stateData.value.appConfig.PARAMETERS_MISSING_BEHAVIOR.data.url
+            break
+        case ParametersMissingBehaviorType.REQUEST_IF_MISSING:
+            console.log('REQUEST_IF_MISSING')
+            console.log('request', stateData.value.appConfig.PARAMETERS_MISSING_BEHAVIOR.data.fields, data, checkIfOtherFieldsFilled(data, stateData.value.appConfig.PARAMETERS_MISSING_BEHAVIOR.data.fields))
+            if (!checkIfOtherFieldsFilled(data, stateData.value.appConfig.PARAMETERS_MISSING_BEHAVIOR.data.fields)) {
+                redirectToHomePage()
+
+                return
+            }
+
+            if (data.roomId !== undefined) {
+                roomDetailsModel.value.roomId = data.roomId
+            }
+
+            if (data.displayName !== undefined) {
+                roomDetailsModel.value.displayName = data.displayName
+            }
+
+            roomDetailsModalOpened.value = true
+            initializingData.value = false
+
+            return
+        default:
+            redirectToHomePage()
+    }
+}
+async function setRoomDetailsData () {
+    await router.push({
+        name: VIDEO_ROOM_CONFERENCE_PAGE_ROUTE.name,
+        query: generateConferenceQueryParameters(roomDetailsModel.value.displayName)
+    })
+
+    roomDetailsModalOpened.value = false
+
+    tryInitializeByQueryParameters()
+}
+
+tryInitializeByQueryParameters()
+</script>
